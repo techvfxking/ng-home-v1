dcd12f0c1c201000f429b1bad2f7136e8908289c
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-10-03 00:33:28 +0530

    further changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideProtractorTestingSupport } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
+import { provideRouter } from '@angular/router';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HomeComponent],
+  providers: [
+    provideProtractorTestingSupport(),
+    provideRouter(AppRoutingModule),
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HomeComponent
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
